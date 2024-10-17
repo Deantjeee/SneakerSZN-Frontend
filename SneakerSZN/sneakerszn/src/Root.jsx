7ee9d92d2c47fcd534dae937d0d077acd5bda7e4
DeanMichiels <PS210022@edu.summacollege.nl>
2024-10-17 14:50:36 +0200

    Navbar finished

--- SneakerSZN/sneakerszn/src/Root.jsx
+++ SneakerSZN/sneakerszn/src/Root.jsx
@@ -1,11 +1,12 @@
 import {Outlet} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import NavBar from "./components/Elements/Navbar";
 
 function Root(){
     return(
         <>
             <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
-            {/* Navbar */}
+            <NavBar/>
             <div className="Content-Box">
                 <Outlet/>
             </div>
