d37bab31721e532278d992131b139da98a1b10d9
DeanMichiels <PS210022@edu.summacollege.nl>
2025-01-14 12:54:26 +0100

    last change to tests

--- SneakerSZN/sneakerszn/tests/UserTests.spec.ts
+++ SneakerSZN/sneakerszn/tests/UserTests.spec.ts
@@ -13,7 +13,6 @@ test('Admin Login', async ({ page }) => {
 
 test('Login with empty email', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('link', { name: 'DASHBOARD' })).toHaveCount(0);
   await page.getByRole('button', { name: 'LOGIN' }).click();
   await page.getByLabel('Your password').fill('test1234');
   await page.getByRole('button', { name: 'LOG IN' }).click();
@@ -31,15 +30,9 @@ test('Login with empty password', async ({ page }) => {
 
 test('User register', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('link', { name: 'DASHBOARD' })).toHaveCount(0);
   await page.getByRole('button', { name: 'REGISTER' }).click();
 
-  await page.getByPlaceholder('name@example.com').fill("test@gmail.com");
-  await page.getByLabel('Your password').fill("test1234");
-  await page.getByLabel('Repeat password').fill("test1234");
-  await page.locator('form').getByRole('button', { name: 'REGISTER' }).click();
-
-  await page.route('*/**/register', async route => {
+  await page.route('**/register', async route => {
     await route.fulfill({
       status: 200,
       contentType: 'application/json',
@@ -47,14 +40,16 @@ test('User register', async ({ page }) => {
     });
   });
 
-  await expect(
-    page.getByRole('alert')
-  ).toHaveText('Successfully signed up!');
+  await page.getByPlaceholder('name@example.com').fill("test1234@gmail.com");
+  await page.getByLabel('Your password').fill("test1234");
+  await page.getByLabel('Repeat password').fill("test1234");
+  await page.locator('form').getByRole('button', { name: 'REGISTER' }).click();
+
+  await expect(page.getByRole('alert')).toHaveText('Successfully signed up!');
 });
 
 test('Register with already in use email', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('link', { name: 'DASHBOARD' })).toHaveCount(0);
   await page.getByRole('button', { name: 'REGISTER' }).click();
   await page.getByPlaceholder('name@example.com').fill("test@gmail.com");
   await page.getByLabel('Your password').fill("test1234");
