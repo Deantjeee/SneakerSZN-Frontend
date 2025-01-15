6669279b383fbf6d8075c340b00e2582b1ce64ac
DeanMichiels <PS210022@edu.summacollege.nl>
2025-01-15 19:42:23 +0100

    -

--- SneakerSZN/sneakerszn/tests/UserTests.spec.ts
+++ SneakerSZN/sneakerszn/tests/UserTests.spec.ts
@@ -51,9 +51,9 @@ test('User register', async ({ page }) => {
 test('Register with already in use email', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'REGISTER' }).click();
-  await page.getByPlaceholder('name@example.com').fill("test@gmail.com");
+  await page.getByPlaceholder('name@example.com').fill("admin@gmail.com");
   await page.getByLabel('Your password').fill("test1234");
   await page.getByLabel('Repeat password').fill("test1234");
   await page.locator('form').getByRole('button', { name: 'REGISTER' }).click();
-  await expect(page.getByText("Username 'test@gmail.com' is already taken.")).toBeVisible();
+  await expect(page.getByText("Username 'admin@gmail.com' is already taken.")).toBeVisible();
 });
